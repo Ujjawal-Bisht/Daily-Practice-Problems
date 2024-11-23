c1ccdff44e48ea4274ee2777fb6c9ddd70aacd91
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-23 22:35:26 +0530

    Day 02

--- DAY_02.py
+++ DAY_02.py
@@ -3,7 +3,11 @@
 """
 Question 03: With a given integral number n, write a program to generate a dictionary that contains (i, i*i) such that is an integral number between 1 and n (both included). and then the program should print the dictionary. Suppose the following input is supplied to the program: 8 Then, the output should be: {1: 1, 2: 4, 3: 9, 4: 16, 5: 25, 6: 36, 7: 49, 8: 64}
 """
-
+n = int(input("Enter number:- "))
+d = dict()
+for i in range(1, n+1):
+    d[i] = i*i
+print(d)
 
 """
 Question 04: Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98 Then, the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98')
