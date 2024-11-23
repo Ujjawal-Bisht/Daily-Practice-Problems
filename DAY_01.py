16befd67dfadc9b71b78fc7ad1606a05cfc30e5a
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-23 22:31:39 +0530

    Day_01

--- DAY_01.py
+++ DAY_01.py
@@ -2,7 +2,7 @@
 
 
 '''
-Question1: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+Question 01: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 '''
 
 for i in range(2000,3001):
@@ -10,7 +10,7 @@ for i in range(2000,3001):
         print(i,end=', ')
 
 '''
-Question2: Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
+Question 02: Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
 '''
 
 def fact(n):
