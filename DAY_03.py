ad21f8f14518d07e0b0a03a32cffdfd4c9f537dd
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-24 21:38:21 +0530

    Day_03

--- DAY_03.py
+++ DAY_03.py
@@ -10,5 +10,18 @@ q = list()
 d = input("Enter numbers:- ").split(',')
 for i in d:
     q.append(str(int(math.sqrt((2*c*int(i))/h))))
-print(','.join(q))
+print(','.join(q))      #Join function will work only for list of strings
+#So, convert list of int into list of strings
 
+"""
+Question 07: Write a program which takes 2 digits, X,Y as input and generates a 2-dimensional array. The element value in the i-th row and j-th column of the array should be i*j. Note: i=0,1.., X-1; j=0,1,¡­Y-1. Example Suppose the following inputs are given to the program: 3,5 Then, the output of the program should be: [[0, 0, 0, 0, 0], [0, 1, 2, 3, 4], [0, 2, 4, 6, 8]]
+"""
+x = int(input("Enter number:- "))
+y = int(input("Enter number:- "))
+l = list()
+for i in range(x):
+    sub_l = list()
+    for j in range(y):
+        sub_l.append(i*j)
+    l.append(sub_l)
+print(l)
