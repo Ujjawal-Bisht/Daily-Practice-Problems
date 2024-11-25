65a9e4b670bc04456ce1ac48ac75bccf6c90c94e
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-25 21:05:25 +0530

    DAY_04

--- DAY_04.py
+++ DAY_04.py
@@ -11,3 +11,8 @@ print(','.join(n))
 """
 Question 09: Write a program that accepts sequence of lines as input and prints the lines after making all characters in the sentence capitalized. Suppose the following input is supplied to the program: Hello world Practice makes perfect Then, the output should be: HELLO WORLD PRACTICE MAKES PERFECT
 """
+ch = True
+while ch == True:
+    n = input("Enter string:- ")
+    print(n.upper())
+    ch = input("Do you want to continue?(True/False) ").capitalize()
