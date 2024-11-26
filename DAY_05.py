c09444f05ffc36e7695b4f0db4e502c3a84774b6
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-26 23:38:22 +0530

    DAY_05

--- DAY_05.py
+++ DAY_05.py
@@ -3,3 +3,8 @@
 """
 Question 10: Write a program that accepts a sequence of whitespace separated words as input and prints the words after removing all duplicate words and sorting them alphanumerically. Suppose the following input is supplied to the program: hello world and practice makes perfect and hello world again Then, the output should be: again and hello makes perfect practice world
 """
+
+n = input("Enter sequence:- ").split(' ')
+sort(n)
+n = list(set(n))
+print(' '.join(n))
