de82ce29ecd9448fb01de40ff7218ddb5b85c987
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-28 20:22:05 +0530

    ques comp

--- DAY_07.py
+++ DAY_07.py
@@ -7,3 +7,10 @@ Question 14: Write a program that accepts a sentence and calculate the number of
 str = input("Enter string:- ")
 u = 0
 l = 0
+for i in str:
+    if i.isupper():
+        u += 1
+    elif i.islower():
+        l += 1
+print("UPPER CASE", u)
+print("LOWER CASE", l)
