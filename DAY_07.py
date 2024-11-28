ee54e56ee1a520f9082589b6b13806e211cfd373
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-28 20:18:34 +0530

    question

--- DAY_07.py
+++ DAY_07.py
@@ -1,1 +1,5 @@
 #   28-11-24
+
+"""
+Question 14: Write a program that accepts a sentence and calculate the number of upper case letters and lower case letters. Suppose the following input is supplied to the program: Hello world! Then, the output should be: UPPER CASE 1 LOWER CASE 9
+"""
