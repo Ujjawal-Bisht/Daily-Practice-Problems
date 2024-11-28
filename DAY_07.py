1e5f425a46c793067ae922c0d33acce72df2f612
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-28 20:32:17 +0530

    next question

--- DAY_07.py
+++ DAY_07.py
@@ -14,3 +14,8 @@ for i in str:
         l += 1
 print("UPPER CASE", u)
 print("LOWER CASE", l)
+
+
+"""
+Question: Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a. Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
+"""
