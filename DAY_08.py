e19fc9b8269ccc4b688a9971f83a4ccb0d976892
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-11-29 22:43:19 +0530

    Ques 2 line 1

--- DAY_08.py
+++ DAY_08.py
@@ -13,3 +13,5 @@ print(res)
 """
 Question 17: Write a program that computes the net amount of a bank account based a transaction log from console input. The transaction log format is shown as following: D 100 W 200
 """
+
+n = input("Enter Transaction:- ").split(' ')
