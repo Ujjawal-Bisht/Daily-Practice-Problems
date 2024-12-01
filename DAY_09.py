12cf48b7a63585659591b3e19235baa3f14513f7
ujjawalbisht <ujjawal.bisht@outlook.com>
2024-12-01 13:10:38 +0530

    change

--- DAY_09.py
+++ DAY_09.py
@@ -11,4 +11,4 @@ Minimum length of transaction password: 6
 Maximum length of transaction password: 12 Your program should accept a sequence of comma separated passwords and will check them according to the above criteria. Passwords that match the criteria are to be printed, each separated by a comma. Example If the following passwords are given as input to the program: ABd1234@1,a F1#,2w3E*,2We3345 Then, the output of the program should be: ABd1234@1
 """
 
-n = input("Enter password:- ")
+n = input("Enter password:- ").split(",")
