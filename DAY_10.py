6741b4dcf1248651ccb570bc2e1dfb51b803ce83
ujjawalbisht <ujjawal.bisht@outlook.com>
2025-01-21 19:03:09 +0530

    DAY_10

---
+++ DAY_10.py
@@ -0,0 +1,9 @@
+for i in range(0,5):
+    for j in range(0,i):
+        print(" ",end="")
+    for k in range(i,5):
+        print("*",end="")
+    for l in range(i,4):
+        print("*",end="")
+    print()
+
